ca2b31ae38ca5ff0ca7489874999645a93170077
hharshagrawal <exp.harsh@gmail.com>
2017-06-09 05:11:44 +0530

    Initial setup

---
+++ src/app/index.js
@@ -0,0 +1,14 @@
+// // import 'styles/styles.scss';
+//
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// // import HelloWorld from 'components/HelloWorld';
+//
+// // const App = () => "<p>HelloWorld</p>";
+//
+// ReactDOM.render(
+//   <p>HelloWorld</p>,
+//   document.getElementById('app')
+// );
+
+console.log("It Works?");
