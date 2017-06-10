5c0d7ecccf2e62120c94e94b36336f1f576a66d8
hharshagrawal <exp.harsh@gmail.com>
2017-06-10 19:59:47 +0530

    Basic Components

--- src/app/index.js
+++ src/app/index.js
@@ -4,11 +4,23 @@ import _ from 'lodash';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { Header } from './components/Header/Header';
+import { Home } from './components/Home';
+
 class App extends React.Component {
     render() {
         return (
-            <div>
-                <h1>First App</h1>
+            <div className="container">
+                <div className="row">
+                    <div className="col-xs-10 col-xs-offset-1">
+                        <Header/>            
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-xs-10 col-xs-offset-1">
+                        <Home/>
+                    </div>
+                </div>
             </div>
         );
     };
